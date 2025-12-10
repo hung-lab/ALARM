5a2e69253448409f730e0c3e5fb3233c3b3dbfd7
Matthew T. Warkentin <warkentin@lunenfeld.ca>
2025-12-10 16:58:14 -0700

    Depend on specific version of flexsurv

--- R/models.R
+++ R/models.R
@@ -1,20 +1,19 @@
 #' Asian Lung Cancer Absolute Risk Models (ALARM)
-#' 
+#'
 #' These functions return ALARM model objects that can be used to make
 #'   predictions of the *t*-year absolute risk of lung cancer mortality. These
 #'   functions are called internally by [`predictALARM()`], if necessary.
-#' 
+#'
 #' @return ALARM model objects (i.e. a `list` with the class `fmsm`).
-#' 
-#' @name ALARM
+#'
 #' @md
-#' 
+#'
 #' @export
 ALARM_NS <- function() {
   readRDS(system.file('models/ALARM_NS.rds', package = 'ALARM'))
 }
 
-#' @rdname ALARM
+#' @rdname ALARM_NS
 #' @export
 ALARM_ES <- function() {
   readRDS(system.file('models/ALARM_ES.rds', package = 'ALARM'))
